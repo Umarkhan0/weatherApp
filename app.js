d5521320f4f9a62beed4ec45767612d6bc296399
Umar khan <125694026+Umarkhan0@users.noreply.github.com>
2023-07-10 14:11:40 +0500

    Add more wether image

--- app.js
+++ app.js
@@ -8,20 +8,33 @@ navigator.geolocation.getCurrentPosition((location) => {
       temp.innerHTML = `
     ${Math.round(res.main.temp)}<sup class="degree">o</sup>C
     `
+
       let back = document.getElementById("image")
       let body = document.getElementById("body");
       if (Math.round(res.main.temp) <= 0) {
-
         back.style.backgroundImage = "url('https://tse4.mm.bing.net/th?id=OIP.UVAyUy5phpeujFgTeWRo_AHaE7&pid=Api&P=0&h=180')";
         body.style.backgroundImage = "url('https://as2.ftcdn.net/v2/jpg/02/42/34/23/1000_F_242342389_NBmK5GZY8PKVSW2ohxEPchaMqCVQIXUW.jpg')"
-
       }
       else if (res.weather[0].main == "Clouds") {
         back.style.backgroundImage = "url('https://wallpaperaccess.com/full/530682.jpg')";
         body.style.backgroundImage = "url('https://j.gifs.com/vQJxxY.gif')"
+      }
+
+      else if (res.weather[0].main == "Thunderstorm") {
+        back.style.backgroundImage = "url('https://rebelfinancial.com/wp-content/uploads/2018/03/vibrant-sunrise-yellow.jpg')";
+        body.style.backgroundImage = "url('https://media.giphy.com/media/elm5ZlD6mCmFa/giphy.gif')"
+      }
+      else if (res.weather[0].main == "Clear") {
+        back.style.backgroundImage = "url('https://rebelfinancial.com/wp-content/uploads/2018/03/vibrant-sunrise-yellow.jpg')";
+        body.style.backgroundImage = "url('https://media.giphy.com/media/lyVNcb1n5Ob0Q/giphy.gif')"
+      }
+      else if (res.weather[0].main == "Smoke") {
+        back.style.backgroundImage = "url('https://thumbs.dreamstime.com/b/shot-out-car-urbanization-global-warming-roadtrip-iceland-smoke-out-geothermal-pipes-against-dramatic-sunset-sky-108272378.jpg')";
+
+        body.style.backgroundImage = "url('https://tse3.mm.bing.net/th?id=OIP.4m3iW4Pk8e1OFQsx6gCD6QHaEK&pid=Api&P=0&h=180')"
 
       }
-      else if (res.weather[0].main === "Rain") {
+      else if (res.weather[0].main == "Rain") {
         back.style.backgroundImage = "url('https://t3.ftcdn.net/jpg/00/70/07/18/240_F_70071801_cv30tSjMxjjleEU9kz7Q43p0pRELYAD0.jpg')";
         body.style.backgroundImage = "url('https://giffiles.alphacoders.com/105/105296.gif')"
 
@@ -31,13 +44,39 @@ navigator.geolocation.getCurrentPosition((location) => {
         back.style.backgroundImage = "url('https://t4.ftcdn.net/jpg/00/55/46/81/240_F_55468141_bnLQvA4ulDX0Jn8ZJq3WxfATKF0Y6rEY.jpg')";
         body.style.backgroundImage = "url('image/images.jpg')";
       }
-      else if (res.weather[0].main == "Clear") {
-        let back = document.getElementById("image")
-        let body = document.getElementById("body");
+      else if (res.weather[0].main == "Fog") {
+        back.style.backgroundImage = "url('https://tse4.mm.bing.net/th?id=OIP.Ta4rHTDuIsCWR7z4BVh4LAHaFj&pid=Api&P=0&h=180')";
+
+        body.style.backgroundImage = "url('https://media.giphy.com/media/VJcvHTPa98IoM/giphy.gif')"
+
+      }
+      else if (res.weather[0].main == "Tornado") {
+        back.style.backgroundImage = "url('https://tse1.mm.bing.net/th?id=OIP.rEot5c_jlK4rfVZBcqXgFgHaE8&pid=Api&P=0&h=180')";
+
+        body.style.backgroundImage = "url('https://api.time.com/wp-content/uploads/2017/04/121.gif')"
+
+      }
+      else if (res.weather[0].main == "Blizzard") {
+        back.style.backgroundImage = "url('https://bgr.com/wp-content/uploads/2015/01/blizzard-2015.jpg?resize=827')";
+
+        body.style.backgroundImage = "url('https://i.pinimg.com/originals/1f/5b/23/1f5b23a579ec85824e4340b20feaa457.gif')"
+
+      }
+      else if (res.weather[0].main == "Sunshine") {
+        back.style.backgroundImage = "url('https://rebelfinancial.com/wp-content/uploads/2018/03/vibrant-sunrise-yellow.jpg')";
+
+        body.style.backgroundImage = "url('https://media1.tenor.com/images/0840f15b8866176a10659fdf0f8f36b7/tenor.gif?itemid=11579869')"
+
+      }
+
+      else if (res.weather[0].main == "Heatwave") {
+
         back.style.backgroundImage = "url('https://www.healthvermont.gov/sites/default/files/images/2018/06/ENV_CH_HotWeather.jpg')";
         body.style.backgroundImage = "url('https://media.giphy.com/media/xT0wlvGLHmojbeu5vq/giphy.gif')";
 
       }
+    
+
       document.getElementById("clouds").innerHTML = `
     ${res.clouds.all}%
     `
@@ -47,7 +86,6 @@ navigator.geolocation.getCurrentPosition((location) => {
       document.getElementById("cityName").innerHTML = `
     ${res.name}<p class="weaterType">${res.weather[0].main}</p>
     `
-
     })
     .catch((err) => {
       console.log(err);
@@ -55,7 +93,7 @@ navigator.geolocation.getCurrentPosition((location) => {
 })
 function search() {
   let user = document.getElementById("inputdisa")
-  console.log(user.value)
+
   fetch(`https://api.openweathermap.org/data/2.5/weather?q=${user.value}&appid=4c6a63ac32f07ed8e5671fc2c70a2de1&units=metric`)
     .then(res => res.json())
     .then((res) => {
@@ -71,14 +109,32 @@ function search() {
         body.style.backgroundImage = "url('https://as2.ftcdn.net/v2/jpg/02/42/34/23/1000_F_242342389_NBmK5GZY8PKVSW2ohxEPchaMqCVQIXUW.jpg')"
 
       }
-      else if (res.weather[0].main == "Clouds") {
+      else if (res.weather[0].main === "Clouds") {
         back.style.backgroundImage = "url('https://wallpaperaccess.com/full/530682.jpg')";
         body.style.backgroundImage = "url('https://j.gifs.com/vQJxxY.gif')"
 
       }
+
+      else if (res.weather[0].main == "Thunderstorm") {
+        back.style.backgroundImage = "url('http://www.weatherforecast.co.uk/blog/wp-content/uploads/2015/06/thunderstorms.jpg')";
+        body.style.backgroundImage = "url('https://media.giphy.com/media/elm5ZlD6mCmFa/giphy.gif')"
+
+      }
+      else if (res.weather[0].main == "Smoke") {
+        back.style.backgroundImage = "url('https://thumbs.dreamstime.com/b/shot-out-car-urbanization-global-warming-roadtrip-iceland-smoke-out-geothermal-pipes-against-dramatic-sunset-sky-108272378.jpg')"
+
+        body.style.backgroundImage = "url('https://tse3.mm.bing.net/th?id=OIP.4m3iW4Pk8e1OFQsx6gCD6QHaEK&pid=Api&P=0&h=180')"
+
+      }
       else if (res.weather[0].main === "Rain") {
-        back.style.backgroundImage = "url('https://t3.ftcdn.net/jpg/00/70/07/18/240_F_70071801_cv30tSjMxjjleEU9kz7Q43p0pRELYAD0.jpg')";
+        back.style.backgroundImage = "url('https://t3.ftcdn.net/jpg/00/70/07/18/240_F_70071801_cv30tSjMxjjleEU9kz7Q43p0pRELYAD0.jpg')"
         body.style.backgroundImage = "url('https://giffiles.alphacoders.com/105/105296.gif')"
+
+      }
+      else if (res.weather[0].main === "Clear") {
+
+        back.style.backgroundImage = "url('https://rebelfinancial.com/wp-content/uploads/2018/03/vibrant-sunrise-yellow.jpg')";
+        body.style.backgroundImage = "url('https://media.giphy.com/media/lyVNcb1n5Ob0Q/giphy.gif')"
 
       }
       else if (res.weather[0].main == "Haze") {
@@ -86,13 +142,38 @@ function search() {
         back.style.backgroundImage = "url('https://t4.ftcdn.net/jpg/00/55/46/81/240_F_55468141_bnLQvA4ulDX0Jn8ZJq3WxfATKF0Y6rEY.jpg')";
         body.style.backgroundImage = "url('image/images.jpg')";
       }
-      else if (res.weather[0].main == "Clear") {
-        let back = document.getElementById("image")
-        let body = document.getElementById("body");
-        back.style.backgroundImage = "url('https://www.healthvermont.gov/sites/default/files/images/2018/06/ENV_CH_HotWeather.jpg')";
-        body.style.backgroundImage = "url('https://media.giphy.com/media/xT0wlvGLHmojbeu5vq/giphy.gif')";
+      else if (res.weather[0].main == "Fog") {
+        back.style.backgroundImage = "url('https://tse4.mm.bing.net/th?id=OIP.Ta4rHTDuIsCWR7z4BVh4LAHaFj&pid=Api&P=0&h=180')"
+
+        body.style.backgroundImage = "url('https://media.giphy.com/media/VJcvHTPa98IoM/giphy.gif')"
 
       }
+      else if (res.weather[0].main == "Tornado") {
+        back.style.backgroundImage = "url('https://tse1.mm.bing.net/th?id=OIP.rEot5c_jlK4rfVZBcqXgFgHaE8&pid=Api&P=0&h=180')"
+
+        body.style.backgroundImage = "url('https://api.time.com/wp-content/uploads/2017/04/121.gif')"
+
+      }
+      else if (res.weather[0].main == "Blizzard") {
+        back.style.backgroundImage = "url('https://bgr.com/wp-content/uploads/2015/01/blizzard-2015.jpg?resize=827')"
+
+        body.style.backgroundImage = "url('https://i.pinimg.com/originals/1f/5b/23/1f5b23a579ec85824e4340b20feaa457.gif')"
+
+      }
+      else if (res.weather[0].main == "Sunshine") {
+        back.style.backgroundImage = "url('https://rebelfinancial.com/wp-content/uploads/2018/03/vibrant-sunrise-yellow.jpg')"
+
+        body.style.backgroundImage = "url('https://media1.tenor.com/images/0840f15b8866176a10659fdf0f8f36b7/tenor.gif?itemid=11579869')"
+
+      }
+
+      else if (res.weather[0].main == "Heatwave") {
+        back.style.backgroundImage = "url('https://www.healthvermont.gov/sites/default/files/images/2018/06/ENV_CH_HotWeather.jpg')"
+        body.style.backgroundImage = "url('https://media.giphy.com/media/xT0wlvGLHmojbeu5vq/giphy.gif')"
+
+      }
+      console.log(res.weather[0].main, back)
+
       document.getElementById("clouds").innerHTML = `
     ${res.clouds.all}%
     `
